aeae8967f06bf7fdc07121d1a0e84a31533ee47c
ravi <ravikanthmp123@gmail.com>
2021-04-23 21:44:38 -0700

    global variables

---
+++ 2-globals.js
@@ -0,0 +1,19 @@
+/**
+ * Globals - No window object
+ * 
+ * 1. __dirname <-- cwd
+ * 2. __filename <-- current filename 
+ * 3. require <-- import modules
+ * 4. modules
+ * 5. process 
+ * 
+ * 
+ */
+
+
+// console.log(__dirname) 
+// console.log(__filename)
+// 
+
+let i = 0;
+setInterval( () => console.log('hello world! ' + i++) , 100)
