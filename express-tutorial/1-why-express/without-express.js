dda8fa3b7ceabc926469efa59b8d20eff11a5972
ravi <ravikanthmp123@gmail.com>
2021-04-25 21:19:13 -0700

    basic setting up ourself

--- express-tutorial/1-why-express/without-express.js
+++ express-tutorial/1-why-express/without-express.js
@@ -2,8 +2,21 @@ const http = require('http')
 
 let server = http.createServer();
 server.on('request', (req, res) => {
-    console.log(req);
-    res.end("vkjbf")
+
+    console.log(req.method);
+    console.log(req.url);
+
+    if (req.url === "/about"){
+        res.writeHead(200, {'content-type' : "text/html"})
+        res.write('<p>About</p>')
+
+    }else{
+        res.writeHead(404, {'content-type' : "text/html"})
+        res.write('<p>Not Found</p>')
+    }
+
+
+    res.end();
 })
 
 server.listen(5000, () => console.log(`server started on port 5000`))
