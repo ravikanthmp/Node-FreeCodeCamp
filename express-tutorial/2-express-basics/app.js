55fa76220b1346193484326d09ef087a25ea2083
ravi <ravikanthmp123@gmail.com>
2021-06-15 21:55:02 -0700

    request params

--- express-tutorial/2-express-basics/app.js
+++ express-tutorial/2-express-basics/app.js
@@ -5,10 +5,16 @@ const app = express();
 
 app.use(express.static('navbar-app'))
 
+// all products
 app.get('/api/products', ((req, res) => {
     res.json(products)
 }))
 
+// get product
+app.get('/api/products/:productId', ((req, res) => {
+    console.log(products.filter(product => product.id === parseInt(req.params.productId)))
+    res.json(products.filter(product => product.id === parseInt(req.params.productId)));
+}))
 
 // default fallback
 app.use('*', ((req, res) => {
